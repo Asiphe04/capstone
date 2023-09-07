5b1572b04f3efadb487e82240aca79c0d24904b2
Asiphe04 <asiphendimlana40@gmail.com>
2023-09-07 14:05:04 +0200

    access control

--- api/index.js
+++ api/index.js
@@ -21,7 +21,9 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Method", "*");
   res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
   next();
 });
 app.use(express.json());
