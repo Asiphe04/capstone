1f3c84ee3f88601b3b57296f68f766882782cf95
Asiphe04 <asiphendimlana40@gmail.com>
2023-09-07 08:21:58 +0200

    ...

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,28 +1,5 @@
 <template>
   <div>
-    <h1>YOUR ITEMS</h1>
-    <div v-if="cart && cart.length > 0">
-      <div v-for="(item, index) in cart" :key="index">
-        <h1>{{ item.prodName }}</h1>
-        <img :src="item.prodURL" alt="" class="w-25" />
-        <h2>Quantity: {{ item.quantity }}</h2>
-        <h3>Price: R{{ item.amount }}</h3>
-
-</table>
+    <h1>Cart</h1>
+  </div>
 </template>
-
-<script>
-   export default {
-    Name: 'setCart',
-    computed: {
-      getCart() {
-        console.log(this.$store.state.cart);
-        return this.$store.state.cart
-      },
-      user(){
-        return this.$store.state.userData
-      }
-    },
-  },
-};
-</script>
