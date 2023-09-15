8c9a62da7d963341e55abb675c877ca4e361b3d4
Asiphe Ndimlana <68120182+Asiphe04@users.noreply.github.com>
2023-09-15 21:07:15 +0200

    cleaning code

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -75,29 +75,7 @@ const createUser = (req, res) => {
     });
   });
 };
-// const createUser = (req, res) => {
-//   const data = req.body;
-//   // Check if userPass is provided in the request body
-//   if (!data.userPass) {
-//     return res.status(400).json({ error: "Password is required." });
-//   }
-//   // Hash the password
-//   data.userPass = bcrypt.hashSync(data.userPass, 10);
-//   const user = {
-//     emailAdd: data.emailAdd,
-//     userPass: data.userPass,
-//   };
-//   let token = createToken(user);
-//   insertUser(data, (err, results) => {
-//     if (err) {
-//       res
-//         .status(500)
-//         .json({ error: "An error occurred while creating the user." });
-//     } else {
-//       res.status(201).json({ token, results });
-//     }
-//   });
-// };
+
 
 // Delete a user
 const deleteUser = (req, res) => {
